d76dc0230f66edda16eb98ec463bd539adb6abcb
tiago-mand <95596343+tiago-mand@users.noreply.github.com>
2022-05-01 11:02:53 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,27 @@
+function soma(numero1, numero2) {
+    let soma =numero1 + numero2;
+    return 'A soma dos dois números é:' + soma;
+}
+    let resultadoDaSoma = soma(1,3)
+    console.log(resultadoDaSoma)
+
+function Subtracao(numero1, numero2) {
+    let subtracao =numero1 - numero2;
+    return 'A subtração dos dois números é:' + subtracao;
+}
+    let resultadoDaSubtracao = Subtracao(5,3)
+    console.log(resultadoDaSubtracao)
+
+function Divisao(numero1, numero2) {
+    let divisao =numero1 / numero2;
+    return 'A divisão dos dois números é:' + divisao;
+}
+    let resultadoDaDivisao= Divisao(8,2)
+    console.log(resultadoDaDivisao)
+
+function Multiplicacao(numero1, numero2) {
+    let Multiplicacao =numero1 * numero2;
+    return 'A Multiplicação dos dois números é:' +Multiplicacao;
+} 
+    let resultadoDaMultiplicacao = Multiplicacao(8,2)
+    console.log(resultadoDaMultiplicacao)
